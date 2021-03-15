6a1ea5578ef9906ded483c97af1bbbfededb26e5
Mayur Somani <mayur491@gmail.com>
2021-03-15 22:51:13 +0530

    first commit

---
+++ src/test/java/com/codemayur/splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemayur.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
